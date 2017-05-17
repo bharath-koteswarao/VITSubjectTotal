b7ce894bccb5337209ef6fe2d883d788ffd34cbe
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-05-17 16:50:06 +0530

    All activities Created

---
+++ app/src/main/java/com/bharath/vitsubjecttotal/TandL.java
@@ -0,0 +1,13 @@
+package com.bharath.vitsubjecttotal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TandL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tand_l);
+    }
+}
