8b196074a9e044593a46a4ab4152dd296e92b285
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-05-17 16:39:38 +0530

    Initial commit

---
+++ app/src/main/java/com/bharath/vitsubjecttotal/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bharath.vitsubjecttotal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void theory(View view) {
+    }
+
+    public void theoryAndLab(View view) {
+    }
+
+    public void theoryAndProject(View view) {
+    }
+
+    public void theoryAndLabAndProject(View view) {
+    }
+}
