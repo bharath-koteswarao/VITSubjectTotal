3c1913d6bba7e6c1fd37c3f6965aebb3b02bac1b
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-12-22 00:17:55 +0530

    Gpa calculator

--- app/src/main/java/com/bharath/vitsubjecttotal/Grades/GpaCalculator.java
+++ app/src/main/java/com/bharath/vitsubjecttotal/Grades/GpaCalculator.java
@@ -1,14 +1,17 @@
 package com.bharath.vitsubjecttotal.Grades;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import com.bharath.vitsubjecttotal.R;
 
 public class GpaCalculator extends AppCompatActivity {
+    RecyclerView gpaRecview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpa_calculator);
+        gpaRecview = (RecyclerView) findViewById(R.id.gpaCalcRecView);
     }
 }
---
+++ app/src/main/java/com/bharath/vitsubjecttotal/Grades/GpaRecview/GpaListItem.java
@@ -0,0 +1,15 @@
+package com.bharath.vitsubjecttotal.Grades.GpaRecview;
+
+/**
+ * Created by bk on 21-12-2017 23:55.
+ */
+
+public class GpaListItem {
+    char grade;
+    int credits;
+
+    public GpaListItem(char grade, int credits) {
+        this.grade = grade;
+        this.credits = credits;
+    }
+}
